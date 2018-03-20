a47ef371c4b91a50ced85a368ed09c117510e3af
Salitha Chathuranga <salithachathuranga94@gmail.com>
2018-03-20 07:29:46 +0530

    Add Database

---
+++ laravel_test.sql
@@ -0,0 +1,267 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 20, 2018 at 02:57 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laravel_test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(6, '2018_03_15_103617_create_tests_table', 4),
+(10, '2014_10_12_000000_create_users_table', 5),
+(11, '2014_10_12_100000_create_password_resets_table', 5),
+(12, '2018_03_03_131325_create_posts_table', 5),
+(13, '2018_03_06_030958_add_user_id_to_posts', 5),
+(14, '2018_03_06_205449_add_cover_image_to_posts', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `password_resets`
+--
+
+INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
+('salithachathuranga94@gmail.com', '$2y$10$R1Zy79I7kS1Y7y9i39iV2e47tI3XZgyHtqPdXxGC8AHluzuiH5tlK', '2018-03-15 14:42:33'),
+('poolsaliya@gmail.com', '$2y$10$KvMmE3SYPFzbd8h7YMMd6.p8KbHUcbPJQwjosglaAUvpUWUkYJFrO', '2018-03-20 00:55:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phones`
+--
+
+CREATE TABLE `phones` (
+  `phone_id` int(5) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `brand` varchar(20) NOT NULL,
+  `dimension` varchar(50) NOT NULL,
+  `sim` varchar(50) NOT NULL,
+  `display_type` varchar(50) NOT NULL,
+  `display_size` varchar(50) NOT NULL,
+  `resolution` varchar(50) NOT NULL,
+  `os` varchar(50) NOT NULL,
+  `chipset` varchar(50) NOT NULL,
+  `cpu` varchar(50) NOT NULL,
+  `gpu` varchar(50) NOT NULL,
+  `cardslot` varchar(50) NOT NULL,
+  `internal` varchar(50) NOT NULL,
+  `ram` varchar(20) NOT NULL,
+  `cam_primary` varchar(40) NOT NULL,
+  `cam_secondary` varchar(40) NOT NULL,
+  `battery` varchar(30) NOT NULL,
+  `image1` varchar(300) NOT NULL,
+  `released_time` varchar(50) NOT NULL,
+  `price` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `phones`
+--
+
+INSERT INTO `phones` (`phone_id`, `name`, `brand`, `dimension`, `sim`, `display_type`, `display_size`, `resolution`, `os`, `chipset`, `cpu`, `gpu`, `cardslot`, `internal`, `ram`, `cam_primary`, `cam_secondary`, `battery`, `image1`, `released_time`, `price`) VALUES
+(1, 'Huawei GR5 2017', 'Huawei', '150.9 x 76.2 x 8.2 mm', 'Hybrid Dual SIM (Nano-SIM, dual stand-by)', 'LTPS IPS LCD 16M colors', '5.5 inches', '1080 x 1920 pixels', 'Android 6.0 (Marshmallow)', 'HiSilicon Kirin 655', 'Octa-core', 'Mali-T830MP2', 'microSD, up to 256 GB', '32 GB', '3 GB RAM', '12 MP + 2MP', '8 MP', '3340mAh', '1.png', '2016, October', '27600 '),
+(2, 'Samsung Galaxy J7 Prime', 'Samsung', '151.7 x 75 x 8 mm', 'Single SIM (Nano-SIM) or Dual SIM', 'PLS TFT 16M colors', '5.5 inches', '1080 x 1920 pixels', 'Android 6.0.1 (Marshmallow)', 'Exynos 7870 Octa', 'Octa-core 1.6 GHz ', 'Mali-T830 MP1', 'microSD, up to 256 GB', '32 GB', '3 GB RAM', '13 MP ', '8 MP', '3300mAh', '2.png', '2016, August', '29800'),
+(3, 'Apple iPhone 7', 'Apple', '138.3 x 67.1 x 7.1 mm', 'Nano-SIM', 'LED-backlit IPS LCD 16M colors', '4.7 inches', '750 x 1334 pixels', 'iOS 10.0.1', 'Apple A10 Fusion', 'Quad-core 2.34 GHz', 'PowerVR Series7XT Plus', 'No', '32 GB', '2 GB RAM', '12 MP', '7 MP ', '1960mAh', '3.png', '2016, September', '110000'),
+(4, 'HTC Desire 628', 'HTC', '146.9 x 70.9 x 8.1 mm', 'Dual SIM (Nano-SIM, dual stand-by)', 'IPS LCD 16M colors', '5.0 inches', '720 x 1280 pixels', 'Android 5.1 (Lollipop)', 'Mediatek MT6753', 'Octa-core 1.3 GHz Cortex-A53', 'Mali-T720MP3', 'microSD, up to 256 GB', '32 GB', '3 GB RAM', '13 MP', '5 MP', '2200mAh', '4.png', '2016, May', '22300'),
+(5, 'Huawei Nova 2', 'Huawei', '142.2 x 68.9 x 6.9 mm', 'Hybrid Dual SIM (Nano-SIM, dual stand-by)', 'LTPS IPS LCD 16M colors', '5.0 inches', '1080 x 1920 pixels', 'Android 7.0 (Nougat)', 'HiSilicon Kirin 659', 'Octa-core', 'Mali-T830 MP2', 'microSD, up to 256 GB', '64 GB', '4 GB RAM', '12 MP + 8 MP', '20 MP', '2950mAh', '5.png', '2017, May', '37300'),
+(6, 'Sony Xperia XZ', 'Sony', '146 x 72 x 8.1 mm', 'Single SIM (Nano-SIM) or Hybrid Dual SIM', 'IPS LCD 16M colors', '5.2 inches', '1080 x 1920 pixels', 'Android 6.0.1 (Marshmallow)', 'Qualcomm MSM8996 Snapdragon 820', 'Quad-core', 'Adreno 530', 'microSD, up to 256 GB', '32 GB', '3 GB RAM', '23 MP', '13MP', '2900mAh', '6.png', '2016, October', '53900 '),
+(7, 'Samsung Galaxy J7 Max', 'Samsung', '148.9 x 71.9 x 7.9 mm', 'Dual SIM (Nano-SIM, dual stand-by)', 'PLS 16M colors', '5.7 inches', '1080 x 1920 pixels', 'Android 7.0 (Nougat)', 'Mediatek MT6757 Helio P20', 'Octa-core 2.4 GHz', 'Mali-T880MP2', 'microSD, up to 256 GB', '32GB', '4GB RAM', '13 MP', '13 MP', '3300mAh', '7.png', '2017, June', ' 32300 '),
+(8, 'HTC ONE A9S ', 'HTC', '146.5 x 71.5 x 8 mm', 'Nano-SIM', 'Super LCD 16M colors', '5.0 inches', '720 x 1280 pixels', 'Android 6.0 (Marshmallow)', 'Mediatek MT6755 Helio P10', 'Octa-core', 'Mali-T860MP2', 'microSD, up to 256 GB', '32GB', '3GB RAM', '13MP', '5 MP', '2300mAh', '8.png', '2016, September', '31900 '),
+(9, 'Sony Xperia XA1', 'SONY', '145 x 67 x 8 mm', 'Single SIM (Nano-SIM) or Dual SIM', 'IPS LCD 16M colors', '5.0 inches', '720 x 1280 pixels', 'Android 7.0 (Nougat)', 'Mediatek MT6757 Helio P20', 'Octa-core', 'Mali-T880MP2', 'microSD, up to 256 GB', '32 GB', '3 GB RAM', '23 MP', '8 MP', '2300mAh', '9.png', '2017, April', '33000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phone_brands`
+--
+
+CREATE TABLE `phone_brands` (
+  `brand_id` int(5) NOT NULL,
+  `brand_name` varchar(20) NOT NULL,
+  `brand_image` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `phone_brands`
+--
+
+INSERT INTO `phone_brands` (`brand_id`, `brand_name`, `brand_image`) VALUES
+(1, 'Huawei', 'huawei.png'),
+(2, 'Apple', 'apple.png'),
+(3, 'Samsung', 'samsung.png'),
+(4, 'HTC', 'htc.png'),
+(5, 'SONY', 'sony.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `body` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cover_image` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `body`, `created_at`, `updated_at`, `user_id`, `username`, `cover_image`) VALUES
+(1, 'Post 1', '<p>xxx</p>', '2018-03-15 15:03:22', '2018-03-15 15:03:59', 1, 'salitha', 'angular_1521146002.jpeg'),
+(2, 'Post 2', '<p>xxxxxxxxxx</p>', '2018-03-16 02:36:00', '2018-03-16 02:36:00', 2, 'Jaadu', 'php_1521187560.jpg'),
+(3, 'Post 3', '<p>aaaaaaaaaaa</p>', '2018-03-16 02:36:58', '2018-03-16 02:36:58', 3, 'palan', 'bootstrap_1521187618.jpeg'),
+(4, 'Post 4', '<p>wwwwwwww</p>', '2018-03-16 02:38:47', '2018-03-16 02:38:47', 1, 'salitha', 'ci_1521187727.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'salitha', 'poolsaliya@gmail.com', '$2y$10$bZPYU6Ky8n5WxqP1Tti4/.8In32TiHG51MUtse/ZkjX4uJE7kJZJC', 'JijynisRIJkcmqmlYaXwMcjojP6NZMJTKJSWjdC6LXokodv0fhrne6E631IQ', '2018-03-15 14:38:53', '2018-03-15 14:38:53'),
+(2, 'Jaadu', 'dewram@gmail.com', '$2y$10$auMNwdk3zkMg2A182kP27exH.bHF4A625Vdh0yxy.QlHMP8IDB8By', '1eeelh0FqoYbh0lzhf4N8xwWEPoDH5nWpIH1Ag9auTUHdWqk5CQeOeZ1hugF', '2018-03-16 02:35:20', '2018-03-16 02:35:20'),
+(3, 'palan', 'lasithahiranjan@gmail.com', '$2y$10$a9LuC56enKfexUsELXVFFO/04.oZKsM46leI8KS6ErCDoGop/C4pS', '7hm3VAIBc8TSCbpUf4UdlT8YygLFLtS9XE0Xenq7cKkcu5KEdEUulU6R6iHU', '2018-03-16 02:36:39', '2018-03-16 02:36:39');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `phones`
+--
+ALTER TABLE `phones`
+  ADD PRIMARY KEY (`phone_id`);
+
+--
+-- Indexes for table `phone_brands`
+--
+ALTER TABLE `phone_brands`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `phones`
+--
+ALTER TABLE `phones`
+  MODIFY `phone_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `phone_brands`
+--
+ALTER TABLE `phone_brands`
+  MODIFY `brand_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
